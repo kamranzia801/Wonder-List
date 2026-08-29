569d236c51bfdf6357c2e591209f0551e5c6c0ad
Kamran Zia <kamranzia801@gmail.com>
2026-08-29 18:27:24 +0530

    created database and made some sort of landing page

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const listing = require("./models/listing.js");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+app.use(express.urlencoded({ extended: true }));
+
+main().then(() => { console.log(`connected to db`) }).catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/wounderlist');
+}
+
+//index route
+
+app.get("/listings", async (req, res) => {
+    const listings = await listing.find({});
+    res.render("./listings/index.ejs", { listings });
+});
+
+//show route
+
+app.get("/listings/:id", async (req, res) => {
+    let { id } = await req.params;
+    const indlisting = await listing.findById(id);
+    res.render("./listings/show.ejs", { indlisting });
+});
+
+
+app.get("/", (req, res) => {
+    res.send(`home page`);
+});
+
+app.listen(8080, (req, res) => {
+    console.log(`app listening to 8080`);
+})
